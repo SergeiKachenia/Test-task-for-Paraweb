2fd1143d4257a6b4e5484967940a92ec4d1bf40e
Sergei Kachenia <sergeikachenya@yandex.ru>
2022-06-27 02:10:28 +0300

    posts almost done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './pages/index.css';
 
 
-const axiosContainer = document.querySelector(".axios-results__grid-container");
+const axiosContainer = document.querySelector(".posts__grid-container");
 
 const getPostsData = () => {
     axios.get('https://mocki.io/v1/a5814d24-4e22-49fc-96d1-0e9ae2952afc')
@@ -9,12 +9,12 @@ const getPostsData = () => {
             console.log(res.data.articles)
             res.data.articles.map((post) => {
                 (axiosContainer.innerHTML += `
-                <div class="axios-results__grid-item">
-                <div class="axios-results__content">
-                <p class="axios-results__data">${post.publishedAt}</p>
-                <p class="axios-results__title">${post.title}</p>
-                <p class="axios-results__description">${post.description}</p>
-              <p class="axios-results__author">${post.author}</p>
+                <div class="post">
+                <div class="post__content">
+                <time class="post__date">${post.publishedAt}</time>
+                <h3 class="post__title">${post.title}</h3>
+                <p class="post__description">${post.description}</p>
+              <p class="post__author">${post.author}</p>
               </div>
               </div>`)
             });
